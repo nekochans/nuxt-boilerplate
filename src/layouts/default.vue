e388045e073fd87a86b9da4e81e2dd9016fc940e
keitakn <keita.koga.work@gmail.com>
2019-02-26 02:54:05 +0900

    :shirt: warning Disallow self-closing on HTML void elementsへの対応
    消す事が出来なかったのでとりあえず<img>を使わない方向で修正

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,14 +3,7 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item">
-          <nuxt-link to="/">
-            <img
-              alt=""
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-            />
-          </nuxt-link>
+          <nuxt-link to="/">🐱トップへ🐱</nuxt-link>
         </a>
       </div>
       <div class="navbar-end">
