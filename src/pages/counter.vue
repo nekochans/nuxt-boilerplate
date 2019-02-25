9591deec33e3ef2b4fa4b6128cd2228ad4cfbf40
keitakn <keita.koga.work@gmail.com>
2019-02-26 01:28:21 +0900

    :sparkles: #10 Counter用のページを追加

---
+++ src/pages/counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="result">
+      🐱が○○匹いる。
+    </div>
+    <button class="button is-info increment">
+      increment
+    </button>
+    <button class="button is-danger decrement">
+      decrement
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  min-height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.result {
+  font-size: 200%;
+}
+
+.increment {
+  margin: 10px;
+}
+
+.decrement {
+  margin: 10px;
+}
+</style>
