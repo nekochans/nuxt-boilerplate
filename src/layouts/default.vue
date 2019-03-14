edaaf1f2bfd554094164a6e0c237d8c8dcf34950
keitakn <keita.koga.work@gmail.com>
2019-03-15 02:39:45 +0900

    :recycle: #10 ClassComponentを書く為に必要だったpackageを削除

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -32,9 +32,3 @@
     </footer>
   </div>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-@Component
-export default class DefaultLayout extends Vue {}
-</script>
